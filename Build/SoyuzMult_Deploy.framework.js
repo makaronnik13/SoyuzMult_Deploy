b2d8470588038f3ea98d1c211e37591093279d29
Makaronnik13 <13nik@mail.ru>
2025-10-02 13:59:05 +0400

    shapoklyak

--- Build/SoyuzMult_Deploy.framework.js
+++ Build/SoyuzMult_Deploy.framework.js
@@ -1752,10 +1752,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6555120: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6555175: ($0) => { performance.now = function() { return $0; }; },  
- 6555223: ($0) => { performance.now = function() { return $0; }; },  
- 6555271: () => { performance.now = Module['emscripten_get_now_backup']; }
+  6556832: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 6556887: ($0) => { performance.now = function() { return $0; }; },  
+ 6556935: ($0) => { performance.now = function() { return $0; }; },  
+ 6556983: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/SoyuzMult_Deploy.loader.js
+++ Build/SoyuzMult_Deploy.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 80419506,
+    wasmFileSize: 80546531,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
